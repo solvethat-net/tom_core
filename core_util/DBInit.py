3b5eb7f7d6f24453e0dde0f6dee3dec65ad75fd4
Sick-E <80492673+Sick-E@users.noreply.github.com>
2022-05-15 22:26:19 +0200

    Install scripts update

--- core_util/DBInit.py
+++ core_util/DBInit.py
@@ -18,28 +18,29 @@
 # USER DATABASE data create
 import os
 import sqlite3
+
 # This script is initializing user database
 # If you need to reset application you can run this script
+if __name__ == "__main__":
+    # This part is creating new user database
+    db_path = str(os.path.dirname(os.path.abspath(__file__))).replace("core_util", "") + "database\\UDB.db"
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
 
-# This part is creating new user database
-db_path = str(os.path.dirname(os.path.abspath(__file__))).replace("core_util", "") + "database\\UDB.db"
-connection = sqlite3.connect(db_path)
-cursor = connection.cursor()
-
-# 1. gui position
-cursor.execute("CREATE TABLE CONFIG(configuration text, value text)")
-cursor.execute("INSERT INTO CONFIG(configuration,value) VALUES('x_pos','0')")
-cursor.execute("INSERT INTO CONFIG(configuration,value) VALUES('y_pos','0')")
-cursor.execute("INSERT INTO CONFIG(configuration,value) VALUES('first_start','0')")
-cursor.execute("INSERT INTO CONFIG(configuration,value) VALUES('language','en')")
-cursor.execute("INSERT INTO CONFIG(configuration,value) VALUES('ocr_threshold','')")
-cursor.execute("INSERT INTO CONFIG(configuration,value) VALUES('current_database','')")
-# 2. paths = all dirs in \\\\user
-cursor.execute("CREATE TABLE PATHS(shortcut text, path text)")
-for one in os.listdir("C:\\Users\\" + os.getlogin()):
-    if os.path.isdir("C:\\Users\\" + os.getlogin() + "\\" + one) and "." not in one:
-        cursor.execute(
-            "INSERT INTO PATHS VALUES('" + one.lower() + "','" + "C:\\Users\\" + os.getlogin() + "\\" + one + "')")
-cursor.execute("CREATE TABLE DATABASES(path text, name text)")
-connection.commit()
-connection.close()
+    # 1. gui position
+    cursor.execute("CREATE TABLE CONFIG(configuration text, value text)")
+    cursor.execute("INSERT INTO CONFIG(configuration,value) VALUES('x_pos','0')")
+    cursor.execute("INSERT INTO CONFIG(configuration,value) VALUES('y_pos','0')")
+    cursor.execute("INSERT INTO CONFIG(configuration,value) VALUES('first_start','0')")
+    cursor.execute("INSERT INTO CONFIG(configuration,value) VALUES('language','en')")
+    cursor.execute("INSERT INTO CONFIG(configuration,value) VALUES('ocr_threshold','')")
+    cursor.execute("INSERT INTO CONFIG(configuration,value) VALUES('current_database','')")
+    # 2. paths = all dirs in \\\\user
+    cursor.execute("CREATE TABLE PATHS(shortcut text, path text)")
+    for one in os.listdir("C:\\Users\\" + os.getlogin()):
+        if os.path.isdir("C:\\Users\\" + os.getlogin() + "\\" + one) and "." not in one:
+            cursor.execute(
+                "INSERT INTO PATHS VALUES('" + one.lower() + "','" + "C:\\Users\\" + os.getlogin() + "\\" + one + "')")
+    cursor.execute("CREATE TABLE DATABASES(path text, name text)")
+    connection.commit()
+    connection.close()
--- core_util/Update.py
+++ core_util/Update.py
@@ -16,21 +16,23 @@
 # https://www.solvethat.net/installer/LICENSE
 
 import os
+import sys
 import git
 
-# Script for app update with git, run from tom_core install dir
 # Check if .git dir exist, when does than do pull or do init when doesn't
 # After running this script is recommended to run pip install -r requirements.txt
-repo_path = os.getcwd()
-if os.path.isdir(repo_path + '\\.git'):
-    empty_repo = git.Repo(repo_path)
-    origin = empty_repo.remote('origin')
-    origin.pull()
-else:
-    empty_repo = git.Repo.init(os.path.join(repo_path))
-    origin = empty_repo.create_remote('origin', 'https://github.com/solvethat-net/tom_core.git')
-    assert origin.exists()
-    assert origin == empty_repo.remotes.origin == empty_repo.remotes['origin']
-    origin.fetch()
-    empty_repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-    origin.pull()
+if __name__ == "__main__":
+    repo_path = sys.argv[1]
+
+    if os.path.isdir(repo_path + '\\.git'):
+        empty_repo = git.Repo(repo_path)
+        origin = empty_repo.remote('origin')
+        origin.pull()
+    else:
+        empty_repo = git.Repo.init(os.path.join(repo_path))
+        origin = empty_repo.create_remote('origin', 'https://github.com/solvethat-net/tom_core.git')
+        assert origin.exists()
+        assert origin == empty_repo.remotes.origin == empty_repo.remotes['origin']
+        origin.fetch()
+        empty_repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+        origin.pull()
